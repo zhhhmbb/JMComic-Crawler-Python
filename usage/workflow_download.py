fc85e87cb8bc8bc5265e76d3299f9b362183c407
zhhhmbb <2105693864@qq.com>
2024-08-13 18:59:40 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,11 +3,12 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-JM437695
-JM270870
-JM264793
-JM542881
-JM495265
+JM293464
+JM521408
+JM568056
+JM299974
+JM531433
+JM265347
 
 '''
 
